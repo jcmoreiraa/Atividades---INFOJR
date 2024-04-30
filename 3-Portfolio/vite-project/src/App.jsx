50fd32bf018910fb9d2492fb490ad2efe3349958
Júlio César <juliomoreira@ufba.br>
2024-04-30 19:45:16 -0300

    header_feita

--- 3-Portfolio/vite-project/src/App.jsx
+++ 3-Portfolio/vite-project/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css'; 
+import VectorImage from './assets/Vector (1).png'; 
+import VectorImagem from './assets/Vector (2).png'; 
+import twitter from './assets/twitter-icon.png';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <body>
+      <header>
+        <div className='kevin'> 
+          <div className='nome'> 
+            <h4> 
+              Kevin Bennet
+            </h4>  
+          </div>
+          <nav className="section_"> 
+            <ul className="section">
+              <li><a href="#section-two">Section Two</a></li>
+              <li><a href="#section-three">Section Three</a></li>
+              <li><a href="#section-four">Section Four</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className="redes_e_botao">
+          <div className="redes">
+        <a href="#"> <img src={twitter} alt="Minha Imagem" /> </a>
+        <a href="#"><img src={VectorImage} alt="Minha Imagem" /></a>
+        <a href='#'> <img src={VectorImagem} alt="Minha Imagem" /> </a></div>
+        
+        <div className="togle">
+          <p className="dm"> Dark mode: </p>
+          <label className="switch">
+  <input type="checkbox"/>
+  <span className="slider round"></span>
+</label>
+        </div>
+        </div>
+      </header>
+    </body>
+  );
 }
 
-export default App
+export default App;
