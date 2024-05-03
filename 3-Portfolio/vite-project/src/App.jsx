f7f97928f85bc34a18939cc87f9c1a2c2f1de100
Júlio César <juliomoreira@ufba.br>
2024-05-02 23:56:53 -0300

    estrutura da atividade 3 feita

--- 3-Portfolio/vite-project/src/App.jsx
+++ 3-Portfolio/vite-project/src/App.jsx
@@ -5,6 +5,8 @@ import VectorImagem from './assets/Vector (2).png';
 import twitter from './assets/twitter-icon.png';
 import avatar from './assets/avatar.png';
 import check from './assets/check.png';
+import julio from './assets/julio cesar eu mesmo (1).jpg';
+import ufba from './assets/ufba (1).png';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,9 +22,9 @@ function App() {
           </div>
           <nav className="section_"> 
             <ul className="section">
-              <li><a href="#section-two">Section Two</a></li>
-              <li><a href="#section-three">Section Three</a></li>
-              <li><a href="#section-four">Section Four</a></li>
+              <li><a id='section' href="#section-two">Section Two</a></li>
+              <li><a id='section' href="#section-three">Section Three</a></li>
+              <li><a id='section' href="#section-four">Section Four</a></li>
             </ul>
           </nav>
         </div>
@@ -31,44 +33,37 @@ function App() {
         <a href="#"> <img src={twitter} alt="Minha Imagem" /> </a>
         <a href="#"><img src={VectorImage} alt="Minha Imagem" /></a>
         <a href='#'> <img src={VectorImagem} alt="Minha Imagem" /> </a></div>
-        
         <div className="togle">
           <p className="dm"> Dark mode: </p>
           <label className="switch">
-  <input type="checkbox"/>
-  <span className="slider round"></span>
-</label>
+            <input type="checkbox"/>
+            <span className="slider round"></span>
+            </label>
+            </div>
+            </div>
+            </header>
+            
+    <main>
+      <section>
+        <div className='first_text'>
+          <div className="x">
+            <img className='minha_foto' src={julio}></img>
+
+          </div>
+          <h1 className='hello'> Hello. <br></br>My name is Júlio César</h1>
+        <div className='linha'></div>
+        <p className='first_text_big'>I'm a Computer Science enthusiast at UFBA, currently participating in an exciting trainee selection process at InfoJr. I'm focused on delivering web programming projects that blend functionality and elegance. My passion lies in simplifying complex processes and creating intuitive digital experiences for end users.<p></p> 
+        As part of my commitment to excellence, I'm constantly honing my skills in various languages, including Python, C++, Ruby, and JavaScript, as well as HTML and CSS. I'm excited to collaborate with a dynamic and diverse team where I can contribute my expertise in interface design and digital product development.<p></p>
+        While my professional journey is just beginning, I'm eager to apply my passion for technology and my desire for continuous learning to tackle exciting challenges and drive team success."</p>
         </div>
-        </div>
-      </header>
+        <div className='avaliable'>
+          <img  className="check" src={check}></img>
+          <p>Available for work and general design goodness – say hello</p>
+          </div>
+          <div className='linha_azul'>
 
-
-      <main>
-
-<section>
-      <div className='first_text'>
-      <div className="x">
-         </div>
-      <h1 className='hello'> Hello. <br></br>My name is Julio</h1>
-       
-       <div className='linha'></div>
-       
-       <p className='first_text_big'>I'm a Digital Product and UI Designer – creating digital experiences that are intuitive for real people and making complex processes easy to use.<p></p> 
-       Right now, I'm Design Lead at Holiday Extras, covering all our digital platforms across a whole load of brands – leading a great team across design, UI engineering, accessibility and customer experience. I'm lucky to be part of a great team of designers, writers, engineers, PMs and data specialists – amongst others – testing, iterating and optimising our platforms and products.<p></p>
-        I've got some work on Dribbble, some previous work at Saga and you can find me over on twitter and sometimes on Medium too. I also take too many photos.</p>
-       </div>
-
-       <div className='avaliable'>
-
-       <img  className="check" src={check}></img>
-
-
-        <p>Available for work and general design goodness – say hello</p>
-       </div>
-       <div className='linha_azul'>
-
-       </div>
-       </section>
+          </div>
+          </section>
 
 
        <section>
@@ -76,33 +71,26 @@ function App() {
         <p className='texto__'>
         Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. 
         </p>
+        <div className='primeira_imagem_texto'> 
 
-
-
-
-
-
-
-
-
-
-
+        <div className='espaço_para_imagem'> </div>
+        <div className='texto_e_titulo'>
+          <h5 className='title'> Title</h5>
+          <p className='leanding'>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
+          <button className='bpl'> Button Primary Light</button>
+          </div>
+        </div> 
+        <div className='segunda_imagem_texto'>
+        <div className='texto_e_titulo'>
+          <h5 className='title'> Title</h5>
+          <p className='leanding'>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
+          <button className='bpl'> Button Primary Light</button> </div>
+        <div className='espaço_para_imagem'>
+        
+        </div>
+        </div>
+          
        </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       </main>
     </body>
   );
