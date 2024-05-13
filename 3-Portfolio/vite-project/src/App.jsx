4c03b42014231abd5f3d876d81ff6fdedd153fe7
Júlio César <juliomoreira@ufba.br>
2024-05-13 19:39:08 -0300

    dark mode feito e SUBLIME

--- 3-Portfolio/vite-project/src/App.jsx
+++ 3-Portfolio/vite-project/src/App.jsx
@@ -9,6 +9,10 @@ import julio from './assets/julio cesar eu mesmo (1).jpg';
 import ufba from './assets/ufba (1).png';
 import menuu from './assets/menu.png';
 import teste from './assets/teste.png';
+import twitter_dark from './assets/Vector (5).png';
+import insta_dark from './assets/Vector (4).png';
+import vector_dark from './assets/Vector (3).png'
+import menu_dark from './assets/Group.png';
 
 function App() {
   const [showSection, setShowSection] = useState(false);
@@ -61,26 +65,29 @@ function App() {
           </nav>
         </div>
         <div className="redes_e_botao">
-          <div className="redes">
-        <a href=""> <img src={twitter} alt="Minha Imagem" /> </a>
-        <a href="#"><img src={VectorImage} alt="Minha Imagem" /></a>
-        <a href='#'> <img src={VectorImagem} alt="Minha Imagem" /> </a></div>
-        <div className="togle">
-          <p className="dm"> Dark mode: </p>
-          <label className="switch">
+           { !isDarkMode && (<div className="redes">
+            <a href=""> <img src={twitter} alt="Minha Imagem" /> </a>
+            <a href="#"><img src={VectorImage} alt="Minha Imagem" /></a>
+            <a href='#'> <img src={VectorImagem} alt="Minha Imagem" /></a></div>)}
+          { isDarkMode && (<div className="redes_dark">
+            <a href=""> <img src={twitter_dark} alt="Minha Imagem" /></a>
+            <a href="#"><img src={vector_dark} alt="Minha Imagem" /></a>
+            <a href='#'> <img src={insta_dark} alt="Minha Imagem" /></a></div>)}
+          <div className="togle">
+            <p className="dm"> Dark mode:</p>
+            <label className="switch">
             <input onClick={toggleDarkMode}  type="checkbox"/>
-            
-            <span className="slider round"></span>
+               <span className="slider round"> </span>
             </label>
-            </div>
-            </div>
-            </header>
+          </div>
+        </div>
+      </header>
             
     <main>
       <section >
-      <button className='menu'><img  className='menu' onClick={toggleShowSection} src={menuu}></img></button>
-
-      { showSection && (
+        {!isDarkMode && (<button className='menu'><img  className='menu' onClick={toggleShowSection} src={menuu}></img></button>)}
+        {isDarkMode && (<button className='menu'><img  className='menu' onClick={toggleShowSection} src={menu_dark}></img></button>)}
+        {showSection && (
             <div className='meu_codigo_esta_uma_bagunça'>
               <nav className="section__"> 
                 <ul className="section_2">
@@ -89,64 +96,64 @@ function App() {
                   <li><a onClick={toggleShowSection} id='section' href="#3">Section Four</a></li>
                 </ul>
               </nav>
-              <div className="redes_">
+              {!isDarkMode && (<div className="redes_">
                 <a href=""> <img src={twitter} alt="Minha Imagem" /> </a>
                 <a href="#"><img src={VectorImage} alt="Minha Imagem" /></a>
-                <a href='#'> <img src={VectorImagem} alt="Minha Imagem" /> </a>
-              </div>
-            </div>
-          )}
+                <a href='#'> <img src={VectorImagem} alt="Minha Imagem" /> </a></div>)}
+              {isDarkMode &&(<div className="redes_dark_">
+                <a href=""> <img src={twitter_dark} alt="Minha Imagem" /> </a>
+                <a href="#"><img src={vector_dark} alt="Minha Imagem" /></a>
+                <a href='#'> <img src={insta_dark} alt="Minha Imagem" /> </a></div>)}
+            </div>)}
           
-        {!showSection && (<div>
+        {!showSection && (
+        <div>
         <div id="1" className='first_text'>
           <div className="x">
             <img className='minha_foto' src={julio}></img>
-
           </div>
           <h1 className='hello'> Hello. <br></br>My name is Júlio César</h1>
-        <div className={!isDarkMode? 'linha': 'linha-dark'}></div>
-        <p className='first_text_big'>I'm a Computer Science enthusiast at UFBA, currently participating in an exciting trainee selection process at InfoJr. I'm focused on delivering web programming projects that blend functionality and elegance. My passion lies in simplifying complex processes and creating intuitive digital experiences for end users.<p></p> 
-        As part of my commitment to excellence, I'm constantly honing my skills in various languages, including Python, C++, Ruby, and JavaScript, as well as HTML and CSS. I'm excited to collaborate with a dynamic and diverse team where I can contribute my expertise in interface design and digital product development.<p></p>
-        While my professional journey is just beginning, I'm eager to apply my passion for technology and my desire for continuous learning to tackle exciting challenges and drive team success.</p>
+          <div className={!isDarkMode? 'linha': 'linha-dark'}></div>
+          <p className='first_text_big'>I'm a Computer Science enthusiast at UFBA, currently participating in an exciting trainee selection process at InfoJr. I'm focused on delivering web programming projects that blend functionality and elegance. My passion lies in simplifying complex processes and creating intuitive digital experiences for end users.<p></p> 
+          As part of my commitment to excellence, I'm constantly honing my skills in various languages, including Python, C++, Ruby, and JavaScript, as well as HTML and CSS. I'm excited to collaborate with a dynamic and diverse team where I can contribute my expertise in interface design and digital product development.<p></p>
+          While my professional journey is just beginning, I'm eager to apply my passion for technology and my desire for continuous learning to tackle exciting challenges and drive team success.</p>
         </div> 
-       <div className={!isDarkMode? 'avaliable': 'avaliable-dark'}>
+        <div className={!isDarkMode? 'avaliable': 'avaliable-dark'}>
           <img  className="check" src={check}></img>
           <p>Available for work and general design goodness – say hello</p>
-          </div>
-          <div className={!isDarkMode? 'linha_azul': 'linha_azul_dark'}>
-
-          </div></div> )}
-          </section>
-
-
-       {!showSection && (<section>
+        </div>
+        <div className={!isDarkMode? 'linha_azul': 'linha_azul_dark'}></div>
+        </div> )}
+      </section>
+      
+      {!showSection && (
+      <section>
         <h1 className='title' id='title'> Title </h1>
         <p className='texto__'>
-        Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. 
+          Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. 
         </p>
         <div id="2" className='primeira_imagem_texto'> 
-
-        <div className='espaço_para_imagem'> </div>
-        <div className='texto_e_titulo'>
-          <h5 className='title'> Title</h5>
-          <p className='leanding'>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
-          <button className='bpl'> Button Primary Light</button>
+          <div className='espaço_para_imagem'> </div>
+          <div className='texto_e_titulo'>
+            <h5 className='title'> Title</h5>
+            <p className='leanding'>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
+            <button className={ !isDarkMode? 'bpl':'bpl_dark'}> {isDarkMode ? 'Button Primary Dark' : 'Button Primary Light'}</button>
+            
           </div>
         </div> 
         <div id="3" className='segunda_imagem_texto'>
-        <div className='texto_e_titulo'>
-          <h5 className='title'> Title</h5>
-          <p className='leanding'>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
-          <button  className='bpl'> Button Primary Light</button> </div>
-        <div className='espaço_para_imagem'>
-        
-        </div>
+          <div className='texto_e_titulo'>
+            <h5 className='title'> Title</h5>
+            <p className='leanding'>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
+            <button className={ !isDarkMode? 'bpl':'bpl_dark'}> {isDarkMode ? 'Button Primary Dark' : 'Button Primary Light'}</button>
+          </div>
+        <div className='espaço_para_imagem'></div>
         </div>
           
-       </section> )}
-      </main>
-    </body>
-  );
+      </section> )}
+    </main>
+  </body>
+);
 }
 
 export default App;
