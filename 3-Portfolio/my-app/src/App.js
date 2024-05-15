6b3b853a6a17f101f350dc034bfc47585d0b66a6
Stefanny <stefannypdao@gmail.com>
2024-05-15 00:16:05 -0300

    responsividade, detalhes em css e html

--- 3-Portfolio/my-app/src/App.js
+++ 3-Portfolio/my-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
         <nav>
           <div className="Left-aligned">
-            <p>Stefanny Oliveira</p>
+            <p className='Nome'>Stefanny Oliveira</p>
             <ul className="Destaque">
               <li>Section two</li>
               <li>Section three</li>
@@ -29,11 +29,12 @@ function App() {
               <li><FiInstagram className="Icon"/></li>
             </ul>
 
-            <p>Dark mode:</p>
-            <button></button>
+            <div className='DarkMode'>
+              <p>Dark mode:</p>
+              <button>toggle</button>
+            </div>
           </div>
         </nav>
-
         
       </header>
 
@@ -47,15 +48,20 @@ function App() {
               </div>
             </div>
 
-            <hr/>
+            <hr className='DivisorUm'/>
 
             <div className="Apresentacao">
               <p>I'm a Digital Product and UI Designer – creating digital experiences that are intuitive for real people and making complex processes easy to use.</p>
               <p>Right now, I'm Design Lead at <span className="Destaque">Holiday Extras</span>, covering all our digital platforms across a whole load of brands – leading a great team across <span className="Destaque">design</span>, UI engineering, accessibility and customer experience. I'm lucky to be part of a great team of designers, writers, engineers, PMs and data specialists – amongst others – testing, iterating and optimising our platforms and products.</p>
-              <p>I've got some <span className="Destaque">work on Dribbble</span>, some previous work at <span className="Destaque">Saga</span> and you can find me over on twitter and sometimes <span className="Destaque">on Medium too</span>. I also take <span className="Destaque">too many photos</span>.
-            </p>
-          </div>
+              <p>I've got some <span className="Destaque">work on Dribbble</span>, some previous work at <span className="Destaque">Saga</span> and you can find me over on twitter and sometimes <span className="Destaque">on Medium too</span>. I also take <span className="Destaque">too many photos</span>.</p>
+            </div>
+
+            <div className='Aviso'>
+              <p>Available for work and general design goodness - <span className='Destaque'>say hello</span></p>
+            </div>
         </div>
+
+        <hr className='DivisorDois'/>
 
         <div className="Conteudo">
           <div className="SectionTwo">
@@ -64,20 +70,20 @@ function App() {
           </div>
 
           <div className="SectionThree">
-            <img src={imagem}/>
+            <img src={imagem} className='Painel'/>
             <div className="Texto">
               <h4>Title</h4>
               <p>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
-              <button className="Destaque">Button Primary Light</button>
+              <button className="Destaque Botao">Button Primary Light</button>
             </div>
           </div>
 
           <div className="SectionFour">
-            <img src={imagem}/>
+            <img src={imagem} className='Painel'/>
             <div className="Texto">
               <h4>Title</h4>
               <p>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
-              <button className="Destaque">Button Primary Light</button>
+              <button className="Destaque Botao">Button Primary Light</button>
             </div>
           </div>
         </div>
